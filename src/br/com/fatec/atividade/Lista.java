552f56ca03b444f0bd391ff83a8ac364c886f083
Karen Vicente <karenyasmin37@gmail.com>
2018-03-20 23:45:18 -0300

    atualizando codigos

--- src/br/com/fatec/atividade/Lista.java
+++ src/br/com/fatec/atividade/Lista.java
@@ -45,10 +45,10 @@ public class Lista {
 	}
 
 	public void sum(Lista lista) {
-		if (lista.inicio != null) {
-			No ultimo = lista.inicio;
+		if (lista.inicio != null) { // tem itens na lista
+			No ultimo = lista.inicio; // No temporario para adicionar os itens
 			do {
-				this.add(ultimo.conteudo);
+				this.add(ultimo.conteudo); // add o item
 				ultimo = ultimo.proximo;
 			} while (ultimo != lista.inicio);
 		}
@@ -56,42 +56,74 @@ public class Lista {
 
 	/**
 	 * Remove o primeiro elemento da lista circular
+	 * 
 	 * @return
 	 */
 	public No remove() {
-		if (inicio == null)
+		if (inicio == null) // lista vazia
 			return null;
-		
-		No temp = inicio;
+
+		No temp = inicio; // no temporário para adicionar os itens
+
+		/**
+		 * esse No é apenas para armazenar/retornar o No que será removido, pois
+		 * ao adicionar um novo No ele será removido
+		 */
 		No temp2 = inicio;
-		
+
+		/**
+		 * caso o No tenha apenas uma posicao e essa seja igual ao inicio, não
+		 * haverá mais lista e o inicio apontara para null
+		 */
 		if (temp.proximo == inicio) {
 			inicio = null;
 			return temp;
 		}
-		
+
+		/**
+		 * percorre e adiciona todos os itens apartir do segundo(afinal o
+		 * primeiro será removido rsrs :))
+		 */
+
 		while (temp.proximo != inicio) {
 			temp = temp.proximo;
 		}
 		temp.proximo = inicio.proximo;
 		inicio = temp.proximo;
 
-		return temp2;
+		return temp2; // retorna o No que foi removido
 	}
-	
+
 	public Lista split(int nro) {
-	
-		return new Lista();
+
+		Lista lista = new Lista();
+		No ultimo = inicio;
+
+		if (inicio == null)
+			return lista;
+
+		do {
+			lista.add(ultimo.conteudo);
+			ultimo = ultimo.proximo;
+		} while (ultimo != inicio);
+
+		return lista;
 	}
 
 	/**
 	 * Interrompe a lista no elemento que foi passado como parâmetro
+	 * 
 	 * @param nro
 	 */
 	public void interrupt(int nro) {
 		if (inicio != null) {
 			No no = inicio;
 
+			/**
+			 * percorre todo o No se(IF) o seu conteudo for diferente do
+			 * enviado, pois os diferente até o numero do item enviado, serão
+			 * armazenados na lista
+			 */
 			while (no.conteudo != nro) {
 				no = no.proximo;
 			}
--- src/br/com/fatec/atividade/exerc5/Principal.java
+++ src/br/com/fatec/atividade/exerc5/Principal.java
@@ -20,5 +20,16 @@ public class Principal {
 		a.add(5);
 		b = b.split(5);
 		b.print();
+		a.add(9);
+		a.add(11);
+		a.add(4);
+		a.add(2);
+		a.add(3);
+		a.add(8);
+		a.add(1);
+		a.print();
+		b = a.split(4);
+		a.print();
+		b.print();
 	}
 }
